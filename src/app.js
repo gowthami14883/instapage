d6648c7f54c3eb3cd63a33acff6c21f550183fbc
Gowthami Nimanagoti <gowthami.nimanagoti@visaka.in>
2026-02-02 17:33:54 +0530

    heyyy

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+// CORS (add HERE)
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
@@ -9,8 +19,7 @@ app.use("/api/followers", require("./routes/follower.routes"));
 app.use("/api/likes", require("./routes/like.routes"));
 app.use("/api/comments", require("./routes/comment.routes"));
 app.use("/api/chats", require("./routes/chat.routes"));
+
 app.use("/uploads", express.static("uploads"));
-
-
 
 module.exports = app;
